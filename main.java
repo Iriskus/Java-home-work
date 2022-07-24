f53661fc1ab08596fac5f84e16a790f3f378bf12
Iriskus <irliatuz@gmail.com>
2022-07-24 16:50:49 -0600

    Create main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int client = 2_000_000_000; // account of client
+        int transfer = 500_000_000; // amount of transfer
+        int total = client + transfer; // total amount of money
+        System.out.println(total);
+    }
+}
